c4991f08af671f13f4c9901c2431a30d2400d424
juanris1977 <120179191+juanris1977@users.noreply.github.com>
2023-02-02 11:43:10 +0100

    2

---
+++ _00_bbdd/src/_00_bbdd/t.java
@@ -0,0 +1,10 @@
+package _00_bbdd;
+
+public class t {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hola");
+	}
+
+}
--- _00_inicio/src/principal/test.java
+++
@@ -1,9 +0,0 @@
-package principal;
-
-public class test {
-
-	public static void main(String[] args) {
-		System.out.println("hola");
-	}
-
-}
